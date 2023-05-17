33e9bb5dc23d6c29e7c8a1a4e95693ec17bc2ae4
Ty Mason <hi@tygr.dev>
2023-05-17 22:05:55 +0100

    you didnt see that...
    Co-Authored-By: NyanSpaghetti <47715070+NyanSpaghetti@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1 className='text-4xl font-bold'>
-        among us
+        Hello world.
       </h1>
     </main>
   )
