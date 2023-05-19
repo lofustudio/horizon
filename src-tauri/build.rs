24e19c495776f0aa78b571fb9880b9b5f354922e
Nyan Spaghetti <47715070+NyanSpaghetti@users.noreply.github.com>
2023-05-19 15:56:46 -0700

    Fix Android builds

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
-  tauri_build::build()
+    #[cfg(target_os = "android")]
+    {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=c++_shared");
+    }
+    tauri_build::build()
+}
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub extern "C" fn __cxa_pure_virtual() {
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(u64::MAX))
+    }
 }
