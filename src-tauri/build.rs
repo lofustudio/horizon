6489ddb00c5aa1ed8a795f8a6e5aa1827ab0a3a2
Ty Mason <hi@tygr.dev>
2023-05-21 11:31:57 +0100

    Revert android workaround

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,13 +1,3 @@
 fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS");
-    if let Ok("android") = target_os.as_ref().map(|x| &**x) {
-        println!("cargo:rustc-link-lib=dylib=stdc++");
-        println!("cargo:rustc-link-lib=c++_shared");
-    }
     tauri_build::build()
-}
-
-#[no_mangle]
-pub extern "C" fn __cxa_pure_virtual() {
-    loop {}
 }
