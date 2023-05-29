cc0044dd7a6ebca51565716d2ca2c6a43b89a587
Ty Mason <hi@tygr.dev>
2023-05-29 15:58:37 +0100

    Fix audio sink
    Co-authored-by: NyanSpaghetti <NyanSpaghetti@users.noreply.github.com>

--- src-tauri/src/audio/mod.rs
+++ src-tauri/src/audio/mod.rs
@@ -1,13 +1,14 @@
-use std::fs::read;
-use std::io::Cursor;
-use std::mem::ManuallyDrop;
+use lofty::{read_from_path, Accessor, AudioFile, TaggedFileExt};
 use rodio::cpal::{self, traits::HostTrait};
 use rodio::{Decoder, Device, OutputStream, Sink};
+use serde::Serialize;
+use serde_json::{json, Value};
+use std::fs::read;
+use std::io::Cursor;
 use std::sync::mpsc::{channel, Sender};
 use std::sync::Mutex;
 use std::thread::spawn;
-use tauri::{AppHandle, command, Manager, State, Wry};
-use serde::Serialize;
+use tauri::{command, AppHandle, Manager, State, Wry};
 
 #[derive(Serialize)]
 pub struct PlaybackState {
@@ -19,10 +20,7 @@ pub struct PlaybackState {
 
 impl PlaybackState {
     fn new(sender: Sender<Device>) -> Self {
-        Self {
-            sink: None,
-            sender
-        }
+        Self { sink: None, sender }
     }
 
     pub fn setup(app: AppHandle<Wry>) {
@@ -35,7 +33,8 @@ impl PlaybackState {
             let mut stream = OutputStream::try_default().expect("No output devices found");
 
             while let Ok(message) = rx.recv() {
-                stream = OutputStream::try_from_device(&message).expect("Failed to use provided Device");
+                stream =
+                    OutputStream::try_from_device(&message).expect("Failed to use provided Device");
                 let sink = Sink::try_new(&stream.1).unwrap();
                 let state_mutex = app.state::<Mutex<PlaybackState>>();
                 let mut state = state_mutex.lock().unwrap();
@@ -44,7 +43,9 @@ impl PlaybackState {
         });
 
         let host = cpal::default_host();
-        let default = host.default_output_device().expect("No output device available");
+        let default = host
+            .default_output_device()
+            .expect("No output device available");
         sender.send(default).expect("Failed to send Device");
     }
 
@@ -54,20 +55,48 @@ impl PlaybackState {
 }
 
 #[command]
-pub async fn play_file(path: String, audio: State<'_, Mutex<PlaybackState>>) -> Result<(), ()> {
+pub async fn play_file(path: String, audio: State<'_, Mutex<PlaybackState>>) -> Result<Value, ()> {
+    let tagged_file = read_from_path(&path).expect("Failed to read from path");
+    // TODO: If primary tag is not found, iterate through the tags to find a suitable one
+    let tag = tagged_file.primary_tag().expect("Primary tag not found");
+
+    println!("--- Tag Information ---");
+    println!("Title: {}", tag.title().as_deref().unwrap_or("None"));
+    println!("Artist: {}", tag.artist().as_deref().unwrap_or("None"));
+    println!("Album: {}", tag.album().as_deref().unwrap_or("None"));
+    println!("Genre: {}", tag.genre().as_deref().unwrap_or("None"));
+
+    let properties = tagged_file.properties();
+
+    println!("Duration: {}s", properties.duration().as_secs());
+
     let file = read(path).expect("Could not read from file");
     let cursor = Decoder::new(Cursor::new(file)).expect("Failed to decode data from file");
-
     audio.lock().unwrap().sink.as_ref().unwrap().append(cursor);
 
-    Ok(())
+    let result = json!({
+        "duration": {
+            "seconds": properties.duration().as_secs(),
+            "ms": properties.duration().as_millis(),
+        },
+        "title": tag.title().as_deref().unwrap_or("None"),
+        "artist": tag.artist().as_deref().unwrap_or("None"),
+        "album": tag.album().as_deref().unwrap_or("None"),
+        "genre": tag.genre().as_deref().unwrap_or("None"),
+    });
+
+    Ok(result)
 }
 
 #[command]
-pub async fn pause(audio: State<'_, Mutex<PlaybackState>>) -> Result<(), ()> {
+pub async fn pause(audio: State<'_, Mutex<PlaybackState>>) -> Result<bool, ()> {
     let playback = audio.lock().unwrap();
     let sink = playback.sink.as_ref().unwrap();
-    if sink.is_paused() { sink.play() } else { sink.pause() }
+    if sink.is_paused() {
+        sink.play()
+    } else {
+        sink.pause()
+    }
 
-    Ok(())
+    Ok(sink.is_paused())
 }
