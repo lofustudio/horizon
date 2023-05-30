b478f74612e725bf62ac65890349df524c034c83
Ty Mason <hi@tygr.dev>
2023-05-30 23:49:05 +0100

    Remove comments

--- src/app.tsx
+++ src/app.tsx
@@ -5,23 +5,14 @@ import PlayerControls from "./components/player/controls"
 
 export default function App({ children }: { children?: React.ReactNode }) {
     useEffect(() => {
-        // define a custom handler function
-        // for the contextmenu event
-        const handleContextMenu = (e: any) => {
-            // prevent the right-click menu from appearing
-            e.preventDefault()
-        }
+        const handleContextMenu = (e: any) => { e.preventDefault() };
 
-        // attach the event listener to 
-        // the document object
         document.addEventListener("contextmenu", handleContextMenu)
 
-        // clean up the event listener when 
-        // the component unmounts
         return () => {
             document.removeEventListener("contextmenu", handleContextMenu)
         }
-    }, [])
+    }, []);
 
     return (
         <>
