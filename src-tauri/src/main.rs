1f97d0ed8a7560d0674f7281ebcb87867552b7d1
Nyan Spaghetti <47715070+NyanSpaghetti@users.noreply.github.com>
2024-01-11 17:42:32 -0800

    feat: use pretty_env_logger

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 
-fn main() {
+use tauri::Manager as _;
+
+#[tokio::main]
+async fn main() {
+    // Initialize logger
+    pretty_env_logger::init();
+
+    // Start Tauri application
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| Ok(()))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
